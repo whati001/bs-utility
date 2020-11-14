8dc08abc6a97d09b0bcc8085c64ef1eba916e814
whati001 <andreas.karner@student.tugraz.at>
2020-11-15 00:49:46 +0100

    AK - Added more UI componets

--- src/lib/input.ts
+++ src/lib/input.ts
@@ -1,8 +1,24 @@
+import { number } from "joi"
 
-export const parseVarName = (input: string): string => {
+export const parseVarName = (input: string, maxLength: number = 20): string => {
   if (input) {
-    const inputFiltered = input.replace(/[^A-Za-z0-9]/, '')
-    return inputFiltered.slice(0, 20)
+    const inputFiltered = input.replace(/[^A-Za-z0-9]/g, '')
+    return inputFiltered.slice(0, maxLength)
   }
   return ''
+}
+
+export const parseText = (input: string, maxLength: number = 100): string => {
+  if (input) {
+    const inputFiltered = input.replace(/[^\x00-\x7F]/g, '')
+    return inputFiltered.slice(0, maxLength)
+  }
+  return ''
+}
+
+export const parseNumber = (input: string | number, max: number = 10): number => {
+  const number = Number(input)
+  if (number && number >= 0)
+    return number <= max ? number : max
+  return 0
 }
--- src/views/full/fillStorage.tsx
+++ src/views/full/fillStorage.tsx
@@ -2,20 +2,23 @@ import React from 'react'
 // @ts-ignore
 import Select from 'react-select'
 // @ts-ignore
-import { Row, Col, Alert, Label, Input } from 'reactstrap'
+import { Container, Row, Col, Alert, Label, Input } from 'reactstrap'
 // @ts-ignore
 import { BotpressTooltip } from 'botpress/tooltip'
+// @ts-ignore
+import style from './style.scss'
 
 import { StorageOption, STORAGES } from './../../lib/storage'
-import { parseVarName } from './../../lib/input'
+import { parseVarName, parseText, parseNumber } from './../../lib/input'
 import { SkillProps } from './../../lib/typing'
 
-const MAX_RETRIES: number = 10;
-
 interface FillStorageState {
-  storage: StorageOption,
-  varName: string,
-  maxRetry: number,
+  storage: StorageOption
+  varName: string
+  maxRetry: number
+  qText: string
+  qOnFailure: string
+  validator: string
   error: string
 }
 
@@ -26,40 +29,54 @@ export class FillStorage extends React.Component<FillStorageProps, FillStorageSt
     storage: undefined,
     varName: '',
     maxRetry: 3,
+    qText: '',
+    qOnFailure: '',
+    validator: '',
     error: undefined
   }
 
-  componentDidMount(): void {
+  componentDidMount = (): void => {
     if (this.props.initialData) {
       this.setStateFromProps(this.props.initialData)
     }
   }
 
-  setStateFromProps(initProps: FillStorageState): void {
+  setStateFromProps = (initProps: FillStorageState): void => {
     const getPropOrDefault = (key: string): any => initProps[key] ? initProps[key] : this.state[key]
 
     this.setState({
       storage: getPropOrDefault('storage'),
       varName: getPropOrDefault('varName'),
       maxRetry: getPropOrDefault('maxRetry'),
+      qText: getPropOrDefault('qText'),
+      qOnFailure: getPropOrDefault('qOnFailure'),
+      validator: getPropOrDefault('validator'),
       error: getPropOrDefault('error')
     })
   }
 
-  hasValidForm(): boolean {
+  hasValidForm = (): boolean => {
     return this.state.storage
       && (this.state.varName != '')
+      && this.state.maxRetry
+      && (this.state.qText != '')
+      && (this.state.qOnFailure != '')
+      && (this.state.validator != '')
   }
 
   componentDidUpdate = (prevProps: FillStorageProps, prevState: FillStorageState): void => {
-    if (this.state != prevState && this.hasValidForm()) {
-      this.updateParent()
+    if (this.state != prevState) {
+      this.updateParent(this.hasValidForm())
     }
   }
 
-  updateParent(): void {
-    this.props.onDataChanged && this.props.onDataChanged(this.state)
-    this.props.onValidChanged && this.props.onValidChanged(true)
+  updateParent = (valid: boolean): void => {
+    if (valid) {
+      this.props.onDataChanged && this.props.onDataChanged(this.state)
+      this.props.onValidChanged && this.props.onValidChanged(true)
+    } else {
+      this.props.onValidChanged && this.props.onValidChanged(false)
+    }
   }
 
 
@@ -67,45 +84,109 @@ export class FillStorage extends React.Component<FillStorageProps, FillStorageSt
     if (STORAGES.includes(selection))
       this.setState({ 'storage': selection })
     else
-      this.setState({ 'error': 'Please select a storage from the selection' })
+      this.setState({ error: 'Please select a storage from the selection' })
   }
 
   handleVarnameChange = (input: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ 'varName': parseVarName(input.target.value) })
+    this.setState({ varName: parseVarName(input.target.value) })
+  }
+
+  handleRetryCount = (input: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ maxRetry: parseNumber(input.target.value) })
+  }
+
+  handleQuestionChange = (input: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ qText: parseText(input.target.value) })
+  }
+
+  handleQuestionOnFailureChange = (input: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ qOnFailure: parseText(input.target.value) })
+  }
+
+  handleValidatorChange = (input: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ validator: parseText(input.target.value) })
   }
 
   render() {
     return (
-      <div className="something">
+      <Container className={style.skillContainer}>
         {this.state.error && <Alert color="danger">{this.state.error}</Alert>}
-        <Row style={{ marginBottom: 10 }}>
-          <Col md={6}>
+        <Row className={style.skillInput}>
+          <Col md={4}>
             <BotpressTooltip message="Select the traget storage type" />
             <Label for="storage">Choose an storage</Label>
             <Select
               id="storage"
               name="storage"
               placeholder="Choose an storage to use"
-              className="something more"
               onChange={this.handleStorageChange}
               value={this.state.storage}
               options={STORAGES}
             />
           </Col>
-          <Col md={6}>
+          <Col md={4}>
             <BotpressTooltip message="Define the variable name, only letters and numbers are allowed" />
             <Label for="varName">Choose a variable name</Label>
             <Input
               id="varName"
               name="varName"
               placeholder="Choose a varName to use"
-              className="something even more"
               onChange={this.handleVarnameChange}
               value={this.state.varName}
             />
           </Col>
+          <Col md={4}>
+            <BotpressTooltip message="Define maximal retry count" />
+            <Label for="maxRetry">Choose a retry count</Label>
+            <Input
+              id="maxRetry"
+              name="maxRetry"
+              placeholder="Choose a retry count"
+              onChange={this.handleRetryCount}
+              value={this.state.maxRetry}
+            />
+          </Col>
         </Row>
-      </div>
+        <Row className={style.skillInput}>
+          <Col md={12}>
+            <BotpressTooltip message="Please enter quesiton to ask" />
+            <Label for="qText">Define question to ask the user</Label>
+            <Input
+              id="qText"
+              name="qText"
+              placeholder="Define some quesition text"
+              onChange={this.handleQuestionChange}
+              value={this.state.qText}
+            />
+          </Col>
+        </Row>
+        <Row className={style.skillInput}>
+          <Col md={12}>
+            <BotpressTooltip message="Please enter quesiton on failure to ask" />
+            <Label for="qText">Define question on failure to ask the user</Label>
+            <Input
+              id="qOnFailure"
+              name="qOnFailure"
+              placeholder="Define some quesition on failure text"
+              onChange={this.handleQuestionOnFailureChange}
+              value={this.state.qOnFailure}
+            />
+          </Col>
+        </Row>
+        <Row className={style.skillInput}>
+          <Col md={12}>
+            <BotpressTooltip message="Please enter validator to verify user input" />
+            <Label for="validator">Define validator action to verify users input</Label>
+            <Input
+              id="validator"
+              name="validator"
+              placeholder="Define some quesition on failure text"
+              onChange={this.handleValidatorChange}
+              value={this.state.validator}
+            />
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
